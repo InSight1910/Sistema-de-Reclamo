eaec33352d2882916038dd9701f27c86c7cd473c
joherrerasep <joherrerasep@gmail.com>
2020-09-25 10:18:10 -0300

    cambios

--- Backend/Reclamos/src/main/java/cl/RECLAMOS/Reclamos/JDBC/ConnectionManager.java
+++ Backend/Reclamos/src/main/java/cl/RECLAMOS/Reclamos/JDBC/ConnectionManager.java
@@ -3,11 +3,22 @@ package cl.RECLAMOS.Reclamos.JDBC;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+/*
 public class ConnectionManager {
     private static Connection connection = null;
     private static String connectionString = "jdbc:sqlserver://localhost:1433; databasename=RECLAMOS; " +
             "IntegratedSecurity=true";
+
+    public static Connection obtenerConexion() throws SQLException {
+        if(connection==null)
+            connection = DriverManager.getConnection(connectionString);
+        return connection;
+    }
+}*/
+public class ConnectionManager {
+    private static Connection connection = null;
+    private static String connectionString = "jdbc:sqlserver://localhost:1433; databasename=RECLAMOS; " +
+            "user=SA; password=<YourStrong@Passw0rd>";
 
     public static Connection obtenerConexion() throws SQLException {
         if(connection==null)
